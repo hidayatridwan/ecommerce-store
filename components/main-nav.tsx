2dca5ccecce87596ca959352d07157286e364a59
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2023-07-27 07:08:16 +0700

    first commit

---
+++ components/main-nav.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Category } from "@/type";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface MainNavProps {
+  data: Category[];
+}
+
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
+  const pathName = usePathname();
+  const routes = data.map((route) => ({
+    href: `category/${route.id}`,
+    label: route.name,
+    active: pathName === `category/${route.id}`,
+  }));
+
+  return (
+    <div>
+      <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
+        {routes.map((route) => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={cn(
+              "text-sm font-medium transitions-color hover:text-black",
+              route.active ? "text-black" : "text-neutral-500"
+            )}
+          >
+            {route.label}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
+};
+
+export default MainNav;
